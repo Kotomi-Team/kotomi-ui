1a1e4d6c5988384a0bf78b3ab73584bff3f2ebbe
zhangj <zhangjin0908@hotmail.com>
2020-01-09 15:20:27 +0800

    fix: 修复代码样式

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 <h1 align="center">Kotomi UI</h1>
 <div align="center">基于Ant Design做的组件扩展，开发的一套组件库</div>
 
-## ✨ 特性 
+## ✨ 特性
 
-- ⚙️  表格支持单元格和行编辑，并且很方便的进行远程分页加载数据。
-- 🌈  语义话的表单定义，无需构建复杂的xml或者json格式。
-- 🛡  使用 TypeScript 开发，提供完整的类型定义文件。
-- 🌍  使用storybook 包含大量例子
+- ⚙️ 表格支持单元格和行编辑，并且很方便的进行远程分页加载数据。
+- 🌈 语义话的表单定义，无需构建复杂的xml或者json格式。
+- 🛡 使用 TypeScript 开发，提供完整的类型定义文件。
+- 🌍 使用storybook 包含大量例子
 
 ## 🖥 支持环境
 
@@ -17,7 +17,6 @@
 | [<img src="https://raw.githubusercontent.com/alrra/browser-logos/master/src/edge/edge_48x48.png" alt="IE / Edge" width="24px" height="24px" />](http://godban.github.io/browsers-support-badges/)</br>IE / Edge | [<img src="https://raw.githubusercontent.com/alrra/browser-logos/master/src/firefox/firefox_48x48.png" alt="Firefox" width="24px" height="24px" />](http://godban.github.io/browsers-support-badges/)</br>Firefox | [<img src="https://raw.githubusercontent.com/alrra/browser-logos/master/src/chrome/chrome_48x48.png" alt="Chrome" width="24px" height="24px" />](http://godban.github.io/browsers-support-badges/)</br>Chrome | [<img src="https://raw.githubusercontent.com/alrra/browser-logos/master/src/safari/safari_48x48.png" alt="Safari" width="24px" height="24px" />](http://godban.github.io/browsers-support-badges/)</br>Safari | [<img src="https://raw.githubusercontent.com/alrra/browser-logos/master/src/opera/opera_48x48.png" alt="Opera" width="24px" height="24px" />](http://godban.github.io/browsers-support-badges/)</br>Opera | [<img src="https://raw.githubusercontent.com/alrra/browser-logos/master/src/electron/electron_48x48.png" alt="Electron" width="24px" height="24px" />](http://godban.github.io/browsers-support-badges/)</br>Electron |
 | --- | --- | --- | --- | --- | --- |
 | IE11, Edge | last 2 versions | last 2 versions | last 2 versions | last 2 versions | last 2 versions |
-
 
 ## 📦 安装
 
@@ -37,7 +36,7 @@ const App = () => (
   <>
     <Form
         script={`
-            [name|Field1 drop 8]        [code|Field2 input 8 ]        [code1|Field3 input 8] 
+            [name|Field1 drop 8]        [code|Field2 input 8 ]        [code1|Field3 input 8]
             [name1|Field4 input 8]       [code2|Field5 input 16-2-22]
             [name2|Field6 input 16-2-22] [code3|Field7 input 8]
         `}
@@ -58,7 +57,7 @@ const App = () => (
 
 - [发布公告](./CHANGELOG.md)
 
-### 🔗 其他链接 
+### 🔗 其他链接
 
 - [Ant Design](http://ant.design/)
 - [组件库](http://ant.design/docs/react/introduce)
--- components/table/ushio/table.ushio.tsx
+++ components/table/ushio/table.ushio.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Table,ColumnProps,TableEvent,TableSorter } from '../Table';
-import { Button } from 'antd';
+import { Button, Checkbox, Select } from 'antd';
 
 export default { title: 'Table' };
 
@@ -105,6 +105,101 @@ export const cellEditorTable = () => {
         dataIndex: 'six',
         title: 'six',
         width: 100
+    },{
+        dataIndex: 'six1',
+        title: 'six1',
+        width: 100
+    },{
+        dataIndex: 'six2',
+        title: 'six2',
+        width: 100
+    },{
+        dataIndex: 'six3',
+        title: 'six3',
+        width: 100
+    },{
+        dataIndex: 'six4',
+        title: 'six4',
+        width: 100
+    }]
+
+    return (
+        <>
+            <Button
+                onClick={()=>{
+                    const dataSourceState = tableDom.getDataSourceState()
+                    console.log(dataSourceState)
+                }}
+            > click get edit state</Button>
+            <Table<User>
+                columns={columns}
+                refExt={(self: any)=>{ tableDom = self}}
+                rowSelection = 'multiple'
+                isEditing={true}
+                event={{
+                onSelect:(selectedRowKeys:string[],selected: boolean)=>{
+                        console.log(selectedRowKeys)
+                        console.log(selected)
+                    },
+                    onRow:(record:User)=>{
+                        return {
+                            onClick:()=>{
+                                console.log(record)
+                            }
+                        }
+                    },
+                    onSave: async (record,type) => {
+                        console.log(record)
+                        console.log(type)
+                        return true
+                    }
+                } as TableEvent<User>}
+                loadData={({page, pageSize}:{page:number,pageSize:number,param?:any,sorter?:TableSorter})=>{
+                    return new Promise<{dataSource:User[],total:number}>((re)=>{
+                        let data:User[] = []
+                        for(let i = 0; i<pageSize! ;i++){
+                            data.push({
+                                'id':`${page} id ${i}`,
+                                'name':`${page} name`,
+                                'six':`${page} six`,
+                                'six1':`${page} six`,
+                                'six2':`${page} six`,
+                                'six3':`${page} six`,
+                                'six4':`${page} six`,
+                            })
+                        }
+                        re({dataSource: data, total: 2000})
+                    })
+                }}
+            />
+        </>
+    )
+}
+
+
+export const cellCheckboxTable = () => {
+    let tableDom : any = undefined 
+
+    let columns: ColumnProps<User>[] = [{
+        dataIndex: '$state',
+        width: 100
+    },{
+        dataIndex: 'name',
+        title: 'name',
+        width: 100,
+        inputType: (
+            <Checkbox value="A">A</Checkbox>
+        )
+    },{
+        dataIndex: 'six',
+        title: 'six',
+        width: 100,
+        inputType: (
+          <Select style={{ width: '100px' }} defaultValue="Home">
+            <Select.Option value="Home">Home</Select.Option>
+            <Select.Option value="Company">Company</Select.Option>
+          </Select>
+        )
     },{
         dataIndex: 'six1',
         title: 'six1',
