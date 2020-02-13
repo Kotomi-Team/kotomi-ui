b02dc032e38f78c56948b6e389ca9617b36eb68b
zhangj <zhangjin19960908@gmail.com>
2020-02-13 17:08:15 +0800

    添加画布

--- components/draw/Canvas.tsx
+++ components/draw/Canvas.tsx
@@ -8,21 +8,11 @@ import "./style/index"
 type Position = {
     x: number
     y: number
+    height?: number
+    width?: number
 }
 
-/**
- * 绘制对应的曲线
- */
-type Line = {
-    // 起始位置 P1
-    p0: Position
-    // 控制点   P2
-    p1: Position
-    // 控制点   P3
-    p2: Position
-    // 结束位置 P4
-    p3: Position
-}
+
 
 type Props = {
 }
@@ -35,6 +25,7 @@ export class Canvas extends React.Component<Props> {
     private canvasElement:React.RefObject<HTMLCanvasElement> = React.createRef<HTMLCanvasElement>()
     private childrenElement:React.RefObject<HTMLDivElement> = React.createRef<HTMLDivElement>()
 
+
     componentDidMount(){
         const offsetHeight = this.childrenElement!.current!.offsetHeight
         const offsetWidth =this.childrenElement!.current!.offsetWidth
@@ -44,29 +35,34 @@ export class Canvas extends React.Component<Props> {
         this.canvasElement.current!.setAttribute('width',`${offsetWidth}px`)
         this.canvasElement.current!.setAttribute('height',`${offsetHeight}px`)
         this.canvasElement.current!.setAttribute('style',style)
-        
         const element = ReactDom.findDOMNode(this) as Element
         element.setAttribute('style',`height:${offsetHeight}px;`)
     }
 
     /**
-     * 绘画出对应的线
+     * 绘制贝塞尔曲线
      * @param line 画出对应的线
      */
-    public paintLine(line: Line):void{
+    public paintBezierLine(from: Position,to: Position):void{
+        const controlPoint: Position =  {x: from.x, y: to.y * 2 }
+        const ctx2d = this.canvasElement.current!.getContext('2d')!
+        ctx2d.beginPath()
+        ctx2d.moveTo(from.x,from.y)
+        ctx2d.quadraticCurveTo(controlPoint.x,controlPoint.y,to.x,to.y)
+        ctx2d.stroke()
     }
 
     /**
-     * 根据ClassName来进行画线
-     * @param from 来源的className
-     * @param to   指向的ClassName
+     * 获取当前HTML元素的坐标信息
+     * @param element 当前HTML元素
      */
-    public paintLineByClassName(from: Element ,to:Element): void{
-        const cxt = this.canvasElement.current!.getContext('2d')!
-        cxt.moveTo(10,10);
-        cxt.lineTo(150,50);
-        cxt.lineTo(10,50);
-        cxt.stroke();
+    public getPositionByElement(element: HTMLElement):Position{
+        return {
+            x: element.offsetLeft - this.canvasElement.current!.offsetLeft,
+            y: element.offsetTop - this.canvasElement.current!.offsetTop,
+            width: element.offsetWidth,
+            height: element.offsetHeight
+        }
     }
 
     render(){
--- tests/Draw.ushio.tsx
+++ tests/Draw.ushio.tsx
@@ -8,10 +8,31 @@ export default { title: 'Draw' };
 
 export const baseTree = () => {
     const canvas = React.createRef<Canvas>()
+    const div0 = React.createRef<HTMLDivElement>()
+    const div1 = React.createRef<HTMLDivElement>()
+    const div2 = React.createRef<HTMLDivElement>()
     return (
         <>
             <Button onClick={()=>{
-                canvas.current!.paintLineByClassName('from','to')
+                const divInfo0 = canvas.current!.getPositionByElement(div0.current!)
+                const divInfo1 = canvas.current!.getPositionByElement(div1.current!)
+                const divInfo2 = canvas.current!.getPositionByElement(div2.current!)
+                canvas.current!.paintBezierLine({
+                    x: divInfo0.x + divInfo0.width!,
+                    y: divInfo0.y + (divInfo0.height!/2),
+                },{
+                    x: divInfo1.x,
+                    y: divInfo1.y + (divInfo1.height!/2),
+                })
+                canvas.current!.paintBezierLine({
+                    x: divInfo0.x + divInfo0.width!,
+                    y: divInfo0.y + (divInfo0.height!/2),
+                },{
+                    x: divInfo2.x,
+                    y: divInfo2.y + (divInfo2.height!/2),
+                })
+                console.log(divInfo0)
+                console.log(divInfo1)
             }}>点击测试绘画</Button>
             <Canvas
                 ref={canvas}
@@ -19,28 +40,36 @@ export const baseTree = () => {
                 <div
                     style={{
                         display: 'flex',
-                        alignItems: 'center'
                     }}
                 >
                     <div 
-                        className="from"
+                        ref={div0}
                         style={{
                             border: "1px solid #1890ff",
                             width: 40,
-                            height: 22
+                            height: 100,
                         }}
                     >你好</div>
 
                     <div 
-                        className="to"
+                        ref={div1}
                         style={{
                             border: "1px solid #1890ff",
                             width: 40,
                             marginLeft: 200,
                             height: 22
                         }}
-                    >再见</div>
+                    >再见0</div>
 
+                    <div 
+                        ref={div2}
+                        style={{
+                            border: "1px solid #1890ff",
+                            width: 40,
+                            marginLeft: 200,
+                            height: 22
+                        }}
+                    >再见1</div>
                 </div>
             </Canvas>
         </>
