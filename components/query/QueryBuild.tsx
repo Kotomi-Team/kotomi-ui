8a426fb30988c616f236db5144504bf9759290dc
jhoneybee <zhangjin19960908@gmail.com>
2020-05-25 23:05:58 +0800

    fix: 修复QueryBuild未正确删除的BUG

--- components/query/QueryBuild.tsx
+++ components/query/QueryBuild.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useImperativeHandle } from 'react'
+import React, { useState, useImperativeHandle, useReducer, useContext } from 'react'
 import * as shortid from 'shortid'
 import { QueryBuild as RcQueryBuild } from 'rc-query'
 import { Query } from 'rc-query/dist/interface'
 import { Input, Select, Col, Icon } from 'antd'
-
 import Dropdown from '../dropdown/Dropdown'
+import { reducer, initialState, State } from './Reducer'
 
 interface DropdownState {
     key: string
@@ -14,181 +14,7 @@ interface DropdownState {
     menus: JSX.Element[]
 }
 
-const queryRender = (
-        type: string,
-        dropState: any,
-        setDropState: any,
-        queryProps: QueryBuildProps,
-        querys: Query[],
-        setQuerys: any,
-        key: string,
-        width: number,
-        height: number,
-
-) => {
-    const menus = [
-        <span key ='AND'>并且</span>,
-        <span key ='OR'>或者</span>,
-        <span key ='NORMAL'>添加条件</span>,
-    ]
-    return (props: any) => {
-        const { style } = props
-        const deleteIcon = (
-            <Icon
-                style={{
-                    position: 'absolute' ,
-                    top: style.top - 7 + (height / 2),
-                    left: style.left + width + 5,
-                }}
-                type="minus-circle"
-                onClick={() => {
-                    // 删除当前节点信息
-                    const loopsDelQuery = (loopsQuery: Query[]) => {
-                        const result: Query[] = []
-                        loopsQuery.forEach((query) => {
-                            if (query.key !== key) {
-                                if (query.children && query.children.length > 0) {
-                                    result.push({
-                                        ...query,
-                                        children: loopsDelQuery(query.children),
-                                    })
-                                }else {
-                                    result.push(query)
-                                }
-                            }
-                        })
-                        return result
-                    }
-                    const newQuery = loopsDelQuery(querys)
-                    setQuerys(newQuery)
-                }}
-             />
-        )
-        if (type === 'AND') {
-            return (
-                <>
-                    <div
-                        style={{
-                            ...style,
-                            textAlign: 'center',
-                            lineHeight: '30px',
-                            border: '1px solid #000',
-                            display: 'inline-block',
-                        }}
-                        onContextMenu={(e) => {
-                            setDropState({
-                                ...dropState,
-                                visible: true,
-                                pageX: e.pageX,
-                                pageY: e.pageY,
-                                key,
-                                menus,
-                            })
-                        }}
-                    >
-                        并且
-                    </div>
-                    {deleteIcon}
-                </>
-            )
-        }
-        else if (type === 'OR') {
-            return (
-                <>
-                    <div
-                        style={{
-                            ...style,
-                            textAlign: 'center',
-                            lineHeight: '30px',
-                            border: '1px solid #000',
-                        }}
-                        onContextMenu={(e) => {
-                            setDropState({
-                                ...dropState,
-                                visible: true,
-                                pageX: e.pageX,
-                                pageY: e.pageY,
-                                key,
-                                menus,
-                            })
-                        }}
-                    >
-                        或者
-                    </div>
-                    {deleteIcon}
-                </>
-            )
-        }
-
-        return (
-            <>
-                <div style={style}>
-                    <QueryInput
-                        fields={queryProps.fields}
-                        symbols={queryProps.symbols}
-                        onChange={(field, symbol, value) => {
-                            let tempQuery: Query | undefined ;
-                            const loopsFindQuery = (tempQuerys: Query[]) => {
-                                tempQuerys.some(element => {
-                                    if (element.children && element.children.length > 0) {
-                                       loopsFindQuery(element.children)
-                                    }
-                                    if (element.key === key) {
-                                        tempQuery = element
-                                        return true
-                                    }
-                                    return false
-                                })
-                            }
-                            loopsFindQuery(querys)
-                            tempQuery!.dataRef = {
-                                field, symbol, value,
-                            }
-
-                        }}
-                    />
-                </div>
-                {deleteIcon}
-            </>
-        )
-    }
-}
-
-const getQuery = (
-        type: string,
-        dropState: any,
-        setDropState: any,
-        queryProps: QueryBuildProps,
-        querys: Query[],
-        setQuerys: any,
-    ) => {
-    const key = shortid.generate()
-    const width = type === 'NORMAL' ? 400 : 60
-    const height = type === 'NORMAL' ? 32 : 30
-    return {
-        key,
-        height,
-        width,
-        margin: {
-            x: 10,
-            y: 20,
-        },
-        render: queryRender(
-            type,
-            dropState,
-            setDropState,
-            queryProps,
-            querys,
-            setQuerys,
-            key,
-            width,
-            height,
-        ),
-        children: [],
-    } as unknown as Query
-}
-
-const loops = (querys: Query[] , cellback: (query: Query) => boolean) => {
+const loops = (querys: Query[], cellback: (query: Query) => boolean) => {
     querys.some((query) => {
         if (query.children && query.children.length > 0) {
             loops(query.children, cellback)
@@ -212,15 +38,13 @@ interface QueryInputProps {
     value?: string
     symbol?: string
 
-    onChange?: (field: string , symbol: string , value: string) => void
+    onChange?: (field: string, symbol: string, value: string) => void
 }
 
 const QueryInput = (props: QueryInputProps) => {
-
     const [field, setField] = useState(props.field)
     const [value, setValue] = useState(props.value)
     const [symbol, setSymbol] = useState(props.symbol)
-
     return (
         <>
             <Input.Group compact>
@@ -275,17 +99,193 @@ interface QueryBuildProps {
     symbols: string[]
 }
 
+interface IContextProps {
+    state: State;
+    dispatch: ({ type, payload }: { type: string, payload: Query[] }) => void;
+}
+const QueryContext = React.createContext({} as IContextProps);
+
+const queryRender = (
+    type: string,
+    dropState: any,
+    setDropState: any,
+    key: string,
+    width: number,
+    height: number,
+    props: QueryBuildProps,
+) => {
+    const menus = [
+        <span key='AND'>并且</span>,
+        <span key='OR'>或者</span>,
+        <span key='NORMAL'>添加条件</span>,
+    ]
+    return ({ style }: { style: any }) => {
+        const { dispatch: rDispatch, state: rState } = useContext(QueryContext)
+        const deleteIcon = (
+            <Icon
+                style={{
+                    position: 'absolute',
+                    top: style.top - 7 + (height / 2),
+                    left: style.left + width + 5,
+                }}
+                type="minus-circle"
+                onClick={() => {
+                    // 删除当前节点信息
+                    const loopsDelQuery = (loopsQuery: Query[]) => {
+                        const result: Query[] = []
+                        loopsQuery.forEach((query) => {
+                            if (query.key === key) {
+                                return
+                            }
+                            if (query.children && query.children.length > 0) {
+                                result.push({
+                                    ...query,
+                                    children: loopsDelQuery(query.children),
+                                })
+                            } else {
+                                result.push(query)
+                            }
+                        })
+                        return result
+                    }
+                    const newQuery = loopsDelQuery(rState.querys)
+
+                    rDispatch({ type: 'SET_QUERYS', payload: newQuery });
+                }}
+            />
+        )
+        if (type === 'AND') {
+            return (
+                <>
+                    <div
+                        style={{
+                            ...style,
+                            textAlign: 'center',
+                            lineHeight: '30px',
+                            border: '1px solid #000',
+                            display: 'inline-block',
+                        }}
+                        onContextMenu={(e) => {
+                            setDropState({
+                                ...dropState,
+                                visible: true,
+                                pageX: e.pageX,
+                                pageY: e.pageY,
+                                key,
+                                menus,
+                            })
+                        }}
+                    >
+                        并且
+            </div>
+                    {deleteIcon}
+                </>
+            )
+        }
+        else if (type === 'OR') {
+            return (
+                <>
+                    <div
+                        style={{
+                            ...style,
+                            textAlign: 'center',
+                            lineHeight: '30px',
+                            border: '1px solid #000',
+                        }}
+                        onContextMenu={(e) => {
+                            setDropState({
+                                ...dropState,
+                                visible: true,
+                                pageX: e.pageX,
+                                pageY: e.pageY,
+                                key,
+                                menus,
+                            })
+                        }}
+                    >
+                        或者
+            </div>
+                    {deleteIcon}
+                </>
+            )
+        }
+
+        return (
+            <>
+                <div style={style}>
+                    <QueryInput
+                        fields={props.fields}
+                        symbols={props.symbols}
+                        onChange={(field, symbol, value) => {
+                            let tempQuery: Query | undefined;
+                            const loopsFindQuery = (tempQuerys: Query[]) => {
+                                tempQuerys.some(element => {
+                                    if (element.children && element.children.length > 0) {
+                                        loopsFindQuery(element.children)
+                                    }
+                                    if (element.key === key) {
+                                        tempQuery = element
+                                        return true
+                                    }
+                                    return false
+                                })
+                            }
+                            loopsFindQuery(rState.querys)
+                            tempQuery!.dataRef = {
+                                field, symbol, value,
+                            }
+
+                        }}
+                    />
+                </div>
+                {deleteIcon}
+            </>
+        )
+    }
+}
+
+const getQuery = (
+    type: string,
+    dropState: any,
+    setDropState: any,
+    queryBuildProps: QueryBuildProps,
+) => {
+    const key = shortid.generate()
+    const width = type === 'NORMAL' ? 400 : 60
+    const height = type === 'NORMAL' ? 32 : 30
+    return {
+        key,
+        height,
+        width,
+        margin: {
+            x: 10,
+            y: 20,
+        },
+        render: queryRender(
+            type,
+            dropState,
+            setDropState,
+            key,
+            width,
+            height,
+            queryBuildProps,
+        ),
+        children: [],
+    } as unknown as Query
+}
+
 export const QueryBuild = React.forwardRef((props: QueryBuildProps, ref: any) => {
-    const [querys, setQuerys] = useState<Query[]>([])
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const { querys } = state
     const [dropState, setDropState] = useState<DropdownState>({
         key: '',
         visible: false,
         pageX: 0,
         pageY: 0,
         menus: [
-            <span key ='AND'>并且</span>,
-            <span key ='OR'>或者</span>,
-            <span key ='NORMAL'>添加条件</span>,
+            <span key='AND'>并且</span>,
+            <span key='OR'>或者</span>,
+            <span key='NORMAL'>添加条件</span>,
         ],
     })
 
@@ -294,7 +294,7 @@ export const QueryBuild = React.forwardRef((props: QueryBuildProps, ref: any) =>
     }))
 
     return (
-        <>
+        <QueryContext.Provider value={{ dispatch, state }}>
             <RcQueryBuild
                 querys={querys}
                 rightClick={(position) => {
@@ -305,8 +305,8 @@ export const QueryBuild = React.forwardRef((props: QueryBuildProps, ref: any) =>
                         pageY: position.y,
                         visible: true,
                         menus: [
-                            <span key ='AND'>并且</span>,
-                            <span key ='OR'>或者</span>,
+                            <span key='AND'>并且</span>,
+                            <span key='OR'>或者</span>,
                         ],
                     })
 
@@ -325,13 +325,11 @@ export const QueryBuild = React.forwardRef((props: QueryBuildProps, ref: any) =>
                 menus={dropState.menus}
                 onClick={(element) => {
                     const key = element.key as string
-                    const tempQuery = getQuery(key, dropState, setDropState, props, querys, setQuerys)
-
+                    const tempQuery = getQuery(key, dropState, setDropState, props)
                     if (dropState.key === '') {
                         querys.push(tempQuery)
-                        setQuerys([...querys])
 
-                    }else {
+                    } else {
                         let cQuery: Query;
                         loops(querys, loopQuery => {
                             if (dropState.key === loopQuery.key) {
@@ -341,14 +339,15 @@ export const QueryBuild = React.forwardRef((props: QueryBuildProps, ref: any) =>
                             return false
                         })
                         cQuery!.children.push(tempQuery)
-                        setQuerys([...querys])
                     }
+
+                    dispatch({ type: 'SET_QUERYS', payload: [...querys] });
                     setDropState({
                         ...dropState,
                         visible: false,
                     })
                 }}
             />
-        </>
+        </QueryContext.Provider>
     )
 })
